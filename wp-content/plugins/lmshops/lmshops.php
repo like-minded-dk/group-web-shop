0fc9b64e030e94e70967d029660a6a5e7b15e849
guandai <guandai@hotmail.com>
2024-02-07 23:24:15 +0000

    add delete create list, move to plugin

---
+++ wp-content/plugins/lmshops/lmshops.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Likeminded Shops
+ * Description: Adds custom shortcodes and styles.
+ * Version: 1.0
+ * Author: Zheng Dai
+ */
+
+include __DIR__.'/list_offer.php';
+include __DIR__.'/create_offer.php';
+
+function my_custom_styles() {
+    wp_enqueue_style('my-custom-styles', plugin_dir_url(__FILE__) . 'css/lmshops.css');
+}
+add_action('wp_enqueue_scripts', 'my_custom_styles');
+
+
+
+# remove breadcrumbs
+add_action( 'init', 'custom_remove_storefront_breadcrumbs');
+
+function custom_remove_storefront_breadcrumbs() {
+    remove_action( 'storefront_before_content', 'woocommerce_breadcrumb', 10 );
+}
