6fdb86409b3ef706e9a6d5d028a8d98f753c1f98
guandai <guandai@hotmail.com>
2024-02-15 02:50:57 +0000

    add wp config

--- wp-config.php
+++ wp-config.php
@@ -6,14 +6,17 @@
  ***/
 
 /* Look up a host-specific config file in
- * /etc/wordpress/config-<host>.php or /etc/wordpress/config-<domain>.php
+ * /usr/share/wordpress/config-<host>.php or /usr/share/wordpress/config-<domain>.php
  */
+
+define('CONFIG_PATH', '/usr/share/wordpress/');
 $debian_server = preg_replace('/:.*/', "", $_SERVER['HTTP_HOST']);
 $debian_server = preg_replace("/[^a-zA-Z0-9.\-]/", "", $debian_server);
-$debian_file = '/etc/wordpress/config-'.strtolower($debian_server).'.php';
+$debian_file = CONFIG_PATH . 'config-'.strtolower($debian_server).'.php';
 /* Main site in case of multisite with subdomains */
 $debian_main_server = preg_replace("/^[^.]*\./", "", $debian_server);
-$debian_main_file = '/etc/wordpress/config-'.strtolower($debian_main_server).'.php';
+$debian_main_file = CONFIG_PATH . 'config-'.strtolower($debian_main_server).'.php';
+
 
 if (file_exists($debian_file)) {
     require_once($debian_file);
@@ -21,9 +24,9 @@ if (file_exists($debian_file)) {
 } elseif (file_exists($debian_main_file)) {
     require_once($debian_main_file);
     define('DEBIAN_FILE', $debian_main_file);
-} elseif (file_exists("/etc/wordpress/config-default.php")) {
-    require_once("/etc/wordpress/config-default.php");
-    define('DEBIAN_FILE', "/etc/wordpress/config-default.php");
+} elseif (file_exists(CONFIG_PATH . "config-default.php")) {
+    require_once(CONFIG_PATH . "config-default.php");
+    define('DEBIAN_FILE', CONFIG_PATH . "config-default.php");
 } else {
     header("HTTP/1.0 404 Not Found");
     echo "Neither <b>$debian_file</b> nor <b>$debian_main_file</b> could be found. <br/> Ensure one of them exists, is readable by the webserver and contains the right password/username.";
@@ -58,6 +61,8 @@ if (!isset($table_prefix)) {
 define( 'SAVEQUERIES', true );
 define( 'SCRIPT_DEBUG', true );
 define( 'WP_DEBUG_LOG', true );
+define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true );
+define( 'WP_DEBUG', true );
 }
 
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
