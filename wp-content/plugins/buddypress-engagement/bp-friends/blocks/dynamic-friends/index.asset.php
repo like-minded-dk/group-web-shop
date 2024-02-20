d9beb15f7af6c9a2822a14c21a072a0759d314a0
guandai <guandai@hotmail.com>
2024-02-20 19:07:53 +0000

    remove useless

--- wp-content/plugins/buddypress-engagement/bp-friends/blocks/dynamic-friends/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('bp-block-data', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '531955e34225e0bc6b2c');
